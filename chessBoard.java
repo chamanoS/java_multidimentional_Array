f70886993d3c4a62ae92e1767b1559c5f7fdd540
chamanos <chamano.sr@gmail.com>
2023-08-17 14:40:55 +0200

    fixing the code

--- chessBoard.java
+++ chessBoard.java
@@ -1,23 +1,25 @@
-import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class chessBoard {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         int boardDim = 8;
         char[][] board = new char[boardDim][boardDim];
         boolean isWhite = false;
-        
-        for(int y = 0; y< board.length; y++){
-            isWhite =!isWhite;
-             for(int x = 0; x< board[y].length; x++){
-            if(isWhite) board[y][x] = 'X';
-            if(!isWhite) board[y][x] = 'M';
+
+        for (int y = 0; y < board.length; y++) {
             isWhite = !isWhite;
-            
-        }
+            for (int x = 0; x < board[y].length; x++) {
+                if (isWhite)
+                    board[y][x] = 'X';
+                if (!isWhite)
+                    board[y][x] = 'M';
+                isWhite = !isWhite;
+
+            }
         }
 
-        for(int i = 0; i<board.length;i++){
-            System.out.println(Array.toString(board));
+        for (int i = 0; i < board.length; i++) {
+            System.out.println(Arrays.toString(board[i]));
         }
     }
 }
